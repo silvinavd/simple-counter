836bb90f2df7d3d5f1f67035de360d5c43278b16
silvinavd <silvinavalverde8@gmail.com>
2021-04-29 19:05:08 +0000

    subo repo inicial

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -6,19 +6,30 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 export function Home() {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		//Contador
+
+		<div className="container d-flex">
+			<div className="box">
+				<i className="far fa-clock fa-5x clock"></i>{" "}
+			</div>
+			<div className="box ">
+				<h1 className="align-items-center">0</h1>
+			</div>
+			<div className="box">
+				<h1>0</h1>
+			</div>
+			<div className="box">
+				<h1>0</h1>
+			</div>
+			<div className="box">
+				<h1>0</h1>
+			</div>
+			<div className="box">
+				<h1>0</h1>
+			</div>
+			<div className="box">
+				<h1>0</h1>
+			</div>
 		</div>
 	);
 }
